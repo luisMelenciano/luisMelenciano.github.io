9187f27eeef4539108accc11cf771e72df8568f7
luisantonio16 <luis16.16m@gmail.com>
2023-04-29 12:23:28 -0400

    update

--- Js/menu.js
+++ Js/menu.js
@@ -1,6 +1,8 @@
 const wp = document.querySelector('.wp');
 const menu = document.querySelector('.nav__items');
-
+const skillsSection = document.querySelector('.skill')
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelectorAll(".progress-bar");
 
 
 wp.addEventListener('click' , ()=>{
@@ -15,4 +17,36 @@ window.addEventListener('click', e=>{
   }
 
 })
+
+function showProgress(){
+  progressBar.forEach(element => {
+    const value = element.dataset.progress;
+    element.style.opacity =1 ;
+    element.style.width = `${value}%`
+    
+  });
+}
+  
+  function hideProgress() {
+    progressBar.forEach(element => {
+      element.style.opacity =0 ;
+      element.style.width = 0
+      
+    });
+    
+}
+
+window.addEventListener('scroll',()=>{
+  const sectionPos = skillsSection.getBoundingClientRect().top;
+  const scrennPos = window.innerHeight / 2;
+
+  if(sectionPos < scrennPos){
+    showProgress();
+     
+  }else{
+
+    hideProgress();
+  }
+})
+
 
